d8d25212bfdeab91546346c04100e7979984c406
ShangXianYue <xieyifan12@gmail.com>
2017-12-22 10:20:19 +0800

    调试错误

--- webhook/sendinfo.py
+++ webhook/sendinfo.py
@@ -8,13 +8,11 @@ from alertwechat import wechat_msg
 def SendAlert(receiver, alert_content, lev=None):
     toemail_list = list()
     tophone_list = list()
-    towechat_list = list()
     send_list = CmsUser.objects.filter(group__name=receiver)
     for send_r in send_list:
         print(send_r)
         toemail_list.append(send_r.email.encode('utf8'))
         tophone_list.append(send_r.phone)
-        towechat_list.append(send_r.wechat_id)
 
     try:
         tomail(toemail_list, alert_content)
@@ -24,13 +22,6 @@ def SendAlert(receiver, alert_content, lev=None):
         tosms(tophone_list, alert_content)
     except:
         print 'send sms error'
-
-    try:
-        wechat_msg(w_id=towechat_list, details=alert_content, level=lev)
-        print(towechat_list)
-        print('send wechating')
-    except:
-        print 'send wechat error'
 
 
 def SendAlert_wechat(receiver, title, startsAt,  status, alert_content, lev=None):
